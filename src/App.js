fc09095888b6c4cd4256316e1572b6a2651687b2
Artur <54365622+bunge12@users.noreply.github.com>
2020-09-18 16:05:08 -0400

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import styled from "styled-components";
+import socketIOClient from "socket.io-client";
+const ENDPOINT = "http://localhost:8001";
+
+const Title = styled.div``;
 
 function App() {
+  // const [game, setGame] = useState(null);
+  useEffect(() => {
+    const io = socketIOClient(ENDPOINT);
+  });
+  const getGame = () => {
+    axios
+      .get("/api/game/3DTU")
+      .then((data) => {
+        console.log(data.data[0]);
+      })
+      .catch((e) => {
+        console.log("Not found");
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title>Trivier</Title>
+      <button onClick={getGame}>Push me</button>
     </div>
   );
 }
---
+++ src/components/Join.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+const Field = styled.input``;
+
+export default function Join(props) {
+  return <Field></Field>;
+}
