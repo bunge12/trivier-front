65fbf6a2a47da2627c2413eea8a1b54d3f4c2186
Artur <54365622+bunge12@users.noreply.github.com>
2020-10-01 10:44:41 -0400

    Combined app.css and index.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import "./App.css";
 import styled from "styled-components";
 import { socket } from "./service/socket";
 import TextInput from "./components/TextInput";
