be0c6a22a58eee2e29a2dbbf1860c1204153eb07
Artur <54365622+bunge12@users.noreply.github.com>
2020-10-27 21:27:49 -0400

    Updated to Google Cloud

--- src/service/socket.js
+++ src/service/socket.js
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-const ENDPOINT = "https://trivier.azurewebsites.net/";
+const ENDPOINT = "https://trivier-a33b2.ue.r.appspot.com/";
 // const ENDPOINT = "http://localhost:8080";
 
 export const socket = io(ENDPOINT);
