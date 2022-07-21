3be7ff1888fb8877a881f3614db1b69351e8acfc
Artur Iatsko <arturyatsko@gmail.com>
2022-07-20 22:17:11 -0400

    update packages & socket URL

--- src/service/socket.js
+++ src/service/socket.js
@@ -1,6 +1,5 @@
 import io from "socket.io-client";
 
-//const ENDPOINT = "https://trivier-a33b2.ue.r.appspot.com/";
-const ENDPOINT = "http://localhost:8080";
+const ENDPOINT = process.env.REACT_APP_BACKEND_URL;
 
 export const socket = io(ENDPOINT);
